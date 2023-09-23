1f5d99dfa6020f9d4685a5210a53075c5f8b4cc7
Edward van Tonder <56725833+EA-Wardie@users.noreply.github.com>
2023-09-23 18:45:12 +0200

    Update AllSessions.vue

--- src/views/AllSessions.vue
+++ src/views/AllSessions.vue
@@ -1,246 +1,313 @@
 <script setup lang="ts">
-    import {
-        IonPage,
-        IonHeader,
-        IonToolbar,
-        IonTitle,
-        IonContent,
-        IonItem,
-        IonLabel,
-        IonButton,
-        IonModal,
-        IonButtons,
-        IonAlert,
-        IonList,
-        IonText,
-        IonSearchbar,
-        IonToast, onIonViewDidEnter,
-    } from '@ionic/vue';
-    import {useSessionsStore} from '@/stores/sessions';
-    import {nextTick, ref} from "vue";
-    import {Ref} from "vue";
-    import {Session} from "@/interfaces/Session";
+  import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonButton,
+    IonModal,
+    IonButtons,
+    IonAlert,
+    IonList,
+    IonText,
+    IonSearchbar,
+    IonToast,
+    onIonViewDidEnter,
+    IonIcon,
+    IonActionSheet,
+  } from '@ionic/vue';
+  import {useSessionsStore} from '@/stores/sessions';
+  import {nextTick, ref} from "vue";
+  import {Ref} from "vue";
+  import {Session} from "@/interfaces/Session";
+  import {ellipsisVertical} from "ionicons/icons";
 
-    const store = useSessionsStore();
-    const sessions: Ref<Session[]> = ref([]);
-    const showDetails: Ref<boolean> = ref(false);
-    const selectedSession: Ref<null | Session> = ref(null);
+  const store = useSessionsStore();
+  const sessions: Ref<Session[]> = ref([]);
+  const showDetails: Ref<boolean> = ref(false);
+  const selectedSession: Ref<null | Session> = ref(null);
 
-    function setSessions() {
-        sessions.value = store.allSessions.sort((a: Session, b: Session) => {
-            if (a.id !== null && b.id !== null) {
-                if (a.id > b.id) {
-                    return -1;
-                } else if (a.id < b.id) {
-                    return 1;
-                }
-
-                return 0;
-            }
-
-            return 0;
-        });
-    }
-
-    onIonViewDidEnter(() => setSessions());
-
-    function filterSessions(event: any): void {
-        const term = event.target.value.toLowerCase().split('-').join('') as string;
-
-        if (term.length > 1) {
-            sessions.value = store.allSessions.filter(
-                ({registration}) =>
-                    registration.toLowerCase().split('-').join('').includes(term)
-                    || registration === ''
-            );
-        } else {
-            setSessions();
+  function setSessions() {
+    sessions.value = store.allSessions.sort((a: Session, b: Session) => {
+      if (a.id !== null && b.id !== null) {
+        if (a.id > b.id) {
+          return -1;
+        } else if (a.id < b.id) {
+          return 1;
         }
+
+        return 0;
+      }
+
+      return 0;
+    });
+  }
+
+  onIonViewDidEnter(() => setSessions());
+
+  function filterSessions(event: any): void {
+    const term = event.target.value.toLowerCase().split('-').join('') as string;
+
+    if (term.length > 1) {
+      sessions.value = store.allSessions.filter(
+          ({registration}) =>
+              registration.toLowerCase().split('-').join('').includes(term)
+              || registration === ''
+      );
+    } else {
+      setSessions();
     }
+  }
 
-    function openDetails(session: Session): void {
-        selectedSession.value = JSON.parse(JSON.stringify(session));
+  const showDeleteAlert: Ref<boolean> = ref(false);
 
-        nextTick(() => {
-            showDetails.value = true;
-        });
-    }
+  function showDeleteAllConfirm() {
+    showDeleteAlert.value = true;
+  }
 
-    function closeDetails(): void {
-        showDetails.value = false;
-    }
+  function closeDeleteAllConfirm() {
+    showDeleteAlert.value = false;
+  }
 
-    const alertButtons: Ref<object[]> = ref([
-        {
-            text: 'Cancel',
-            role: 'destructive',
-        },
-        {
-            text: 'Delete',
-            role: 'confirm',
-            handler: (): void => {
-                deleteSession();
-            },
-        },
-    ]);
+  function deleteAllSession() {
+    closeDeleteAllConfirm();
+    store.deleteAll();
+    setSessions();
+  }
 
-    function deleteSession(): void {
-        store.delete(selectedSession.value?.id ?? null).then(() => {
-            setSessions();
-            showSnackbar();
-        });
+  const actionSheetButtons = [
+    {
+      text: 'Delete All Sessions',
+      role: 'destructive',
+      handler: (): void => {
+        showDeleteAllConfirm();
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
 
-        nextTick(() => showDetails.value = false);
-    }
+  const alertDeleteAllButtons: Ref<object[]> = ref([
+    {
+      text: 'Cancel',
+      role: 'destructive',
+    },
+    {
+      text: 'Delete',
+      role: 'confirm',
+      handler: (): void => {
+        deleteAllSession();
+      },
+    },
+  ]);
 
-    const snackbar: Ref<boolean> = ref(false);
+  function openDetails(session: Session): void {
+    selectedSession.value = JSON.parse(JSON.stringify(session));
 
-    function showSnackbar(): void {
-        snackbar.value = true;
-    }
+    nextTick(() => {
+      showDetails.value = true;
+    });
+  }
 
-    function closeSnackbar(): void {
-        snackbar.value = false;
-    }
+  function closeDetails(): void {
+    showDetails.value = false;
+  }
+
+  const alertButtons: Ref<object[]> = ref([
+    {
+      text: 'Cancel',
+      role: 'destructive',
+    },
+    {
+      text: 'Delete',
+      role: 'confirm',
+      handler: (): void => {
+        deleteSession();
+      },
+    },
+  ]);
+
+  function deleteSession(): void {
+    store.delete(selectedSession.value?.id ?? null).then(() => {
+      setSessions();
+      showSnackbar();
+    });
+
+    nextTick(() => showDetails.value = false);
+  }
+
+  const snackbar: Ref<boolean> = ref(false);
+
+  function showSnackbar(): void {
+    snackbar.value = true;
+  }
+
+  function closeSnackbar(): void {
+    snackbar.value = false;
+  }
 </script>
 
 <template>
-    <ion-page>
-        <ion-header>
-            <ion-toolbar>
-                <ion-title>All Sessions</ion-title>
-            </ion-toolbar>
-            <ion-toolbar v-show="store.hasSessions">
-                <ion-searchbar
-                    animated
-                    show-clear-button="focus"
-                    placeholder="Find Session..."
-                    :debounce="1000"
-                    @input="filterSessions"
-                ></ion-searchbar>
-            </ion-toolbar>
-        </ion-header>
-        <ion-content :fullscreen="true">
-            <ion-list inset lines="full" v-if="store.hasSessions">
-                <ion-item :key="session.id as number" v-for="(session) in sessions">
-                    <ion-label>
-                        <h3 :class="[session.registration ? 'ion-text-uppercase' : '']">
-                            {{ session.registration || 'No Registration' }}
-                        </h3>
-                        <p>{{ session.start }}</p>
-                    </ion-label>
-                    <ion-button slot="end" @click="openDetails(session)">Details</ion-button>
-                </ion-item>
-            </ion-list>
-            <div class="expanded flex column ion-justify-content-center ion-align-items-center" v-else>
-                <ion-text>
-                    <h3>No Sessions Yet</h3>
-                </ion-text>
-                <ion-text>
-                    <h6>Start by creating your first session.</h6>
-                </ion-text>
-            </div>
-        </ion-content>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>All Sessions</ion-title>
+        <ion-buttons slot="primary">
+          <ion-button id="open-sessions-actions">
+            <ion-icon slot="icon-only" :md="ellipsisVertical"></ion-icon>
+          </ion-button>
+          <ion-action-sheet
+              trigger="open-sessions-actions"
+              header="Actions"
+              :buttons="actionSheetButtons"
+          ></ion-action-sheet>
+          <ion-alert
+              header="Confirm"
+              sub-header="Delete All Session"
+              message="Are you sure you would like to delete all sessions. This action is final"
+              :is-open="showDeleteAlert"
+              :buttons="alertDeleteAllButtons"
+              @didDismiss="closeDeleteAllConfirm"
+          ></ion-alert>
+        </ion-buttons>
+      </ion-toolbar>
+      <ion-toolbar v-show="store.hasSessions">
+        <ion-searchbar
+            animated
+            show-clear-button="focus"
+            placeholder="Find Session..."
+            :debounce="1000"
+            @input="filterSessions"
+        ></ion-searchbar>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-list inset lines="full" v-if="store.hasSessions">
+        <ion-item :key="session.id as number" v-for="(session) in sessions">
+          <ion-label>
+            <h3 :class="[session.registration ? 'ion-text-uppercase' : '']">
+              {{ session.registration || 'No Registration' }}
+            </h3>
+            <p>{{ session.start }}</p>
+          </ion-label>
+          <ion-button slot="end" @click="openDetails(session)">Details</ion-button>
+        </ion-item>
+      </ion-list>
+      <div class="expanded flex column ion-justify-content-center ion-align-items-center" v-else>
+        <ion-text>
+          <h3>No Sessions Yet</h3>
+        </ion-text>
+        <ion-text>
+          <h6>Start by creating your first session.</h6>
+        </ion-text>
+      </div>
+    </ion-content>
 
-        <ion-modal :is-open="showDetails" @didDismess="closeDetails">
-            <ion-header>
-                <ion-toolbar>
-                    <ion-title>Session Details</ion-title>
-                    <ion-buttons slot="end">
-                        <ion-button :strong="true" @click="closeDetails">Close</ion-button>
-                    </ion-buttons>
-                </ion-toolbar>
-            </ion-header>
-            <ion-content v-if="selectedSession !== null">
-                <ion-list inset lines="full">
-                    <ion-item>
-                        <ion-label>Aircraft Registration</ion-label>
-                        <ion-label slot="end" class="ion-text-uppercase">
-                            <b>{{ selectedSession.registration || '-' }}</b>
-                        </ion-label>
-                    </ion-item>
+    <ion-modal :is-open="showDetails" @didDismess="closeDetails">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Session Details</ion-title>
+          <ion-buttons slot="end">
+            <ion-button :strong="true" @click="closeDetails">Close</ion-button>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content v-if="selectedSession !== null">
+        <ion-list inset lines="full">
+          <ion-item>
+            <ion-label>Aircraft Registration</ion-label>
+            <ion-label slot="end" class="ion-text-uppercase">
+              <b>{{ selectedSession.registration || '-' }}</b>
+            </ion-label>
+          </ion-item>
 
-                    <ion-item>
-                        <ion-label>Created</ion-label>
-                        <ion-label slot="end">
-                            <p>{{ selectedSession.start }}</p>
-                        </ion-label>
-                    </ion-item>
+          <ion-item>
+            <ion-label>Created</ion-label>
+            <ion-label slot="end">
+              <p>{{ selectedSession.start }}</p>
+            </ion-label>
+          </ion-item>
 
-                    <ion-item>
-                        <ion-label>Total Time</ion-label>
-                        <ion-label slot="end">
-                            <p>{{
-                                    selectedSession.shutoff
-                                }}</p>
-                        </ion-label>
-                    </ion-item>
+          <ion-item>
+            <ion-label>Total Time</ion-label>
+            <ion-label slot="end">
+              <p>{{ selectedSession.shutoff }}</p>
+            </ion-label>
+          </ion-item>
 
-                    <ion-item>
-                        <ion-label>Engine Start</ion-label>
-                        <ion-label slot="end">
-                            <p>{{ selectedSession.start }}</p>
-                        </ion-label>
-                    </ion-item>
+          <ion-item>
+            <ion-label>Engine Start</ion-label>
+            <ion-label slot="end">
+              <p>{{ selectedSession.start }}</p>
+            </ion-label>
+          </ion-item>
 
-                    <ion-item>
-                        <ion-label>Taxi Start</ion-label>
-                        <ion-label slot="end">
-                            <p>{{ selectedSession.taxi }}</p>
-                        </ion-label>
-                    </ion-item>
+          <ion-item>
+            <ion-label>Taxi Start</ion-label>
+            <ion-label slot="end">
+              <p>{{ selectedSession.taxi }}</p>
+            </ion-label>
+          </ion-item>
 
-                    <ion-item>
-                        <ion-label>Takeoff</ion-label>
-                        <ion-label slot="end">
-                            <p>{{ selectedSession.takeoff }}</p>
-                        </ion-label>
-                    </ion-item>
+          <ion-item>
+            <ion-label>Takeoff</ion-label>
+            <ion-label slot="end">
+              <p>{{ selectedSession.takeoff }}</p>
+            </ion-label>
+          </ion-item>
 
-                    <ion-item>
-                        <ion-label>Landed</ion-label>
-                        <ion-label slot="end">
-                            <p>{{ selectedSession.land }}</p>
-                        </ion-label>
-                    </ion-item>
+          <ion-item>
+            <ion-label>Landed</ion-label>
+            <ion-label slot="end">
+              <p>{{ selectedSession.land }}</p>
+            </ion-label>
+          </ion-item>
 
-                    <ion-item>
-                        <ion-label>Engine Shutoff</ion-label>
-                        <ion-label slot="end">
-                            <p>{{ selectedSession.shutoff }}</p>
-                        </ion-label>
-                    </ion-item>
-                </ion-list>
+          <ion-item>
+            <ion-label>Engine Shutoff</ion-label>
+            <ion-label slot="end">
+              <p>{{ selectedSession.shutoff }}</p>
+            </ion-label>
+          </ion-item>
+        </ion-list>
 
-                <div class="ion-padding-horizontal">
-                    <ion-button
-                        id="trigger-delete-confirm"
-                        class="ion-margin-top"
-                        color="danger"
-                        expand="block"
-                    >Delete Session
-                    </ion-button>
-                </div>
+        <div class="ion-padding-horizontal">
+          <ion-button
+              id="trigger-delete-confirm"
+              class="ion-margin-top"
+              color="danger"
+              expand="block"
+          >Delete Session
+          </ion-button>
+        </div>
 
-                <ion-alert
-                    trigger="trigger-delete-confirm"
-                    header="Confirm"
-                    sub-header="Delete Session"
-                    message="Are you sure you would like to delete this session"
-                    :buttons="alertButtons"
-                ></ion-alert>
-            </ion-content>
-        </ion-modal>
+        <ion-alert
+            trigger="trigger-delete-confirm"
+            header="Confirm"
+            sub-header="Delete Session"
+            message="Are you sure you would like to delete this session"
+            :buttons="alertButtons"
+        ></ion-alert>
+      </ion-content>
+    </ion-modal>
 
-        <ion-toast
-            message="Session successfully deleted"
-            position="top"
-            color="danger"
-            :is-open="snackbar"
-            :duration="2000"
-            @didDismiss="closeSnackbar()"
-        ></ion-toast>
-    </ion-page>
+    <ion-toast
+        message="Session successfully deleted"
+        position="top"
+        color="danger"
+        :is-open="snackbar"
+        :duration="2000"
+        @didDismiss="closeSnackbar()"
+    ></ion-toast>
+  </ion-page>
 </template>
 
 <style scoped>
